355956ca857663994019c08e95f39ce357c310ed
Matthias Kruk <m@m10k.eu>
2024-12-15 12:34:43 +0900

    config: Add table with key bindings
    To invoke commands when the user presses keys, a mapping from inputs
    to commands is needed.
    
    This commit adds a table that is used to configure key bindings.

---
+++ src/config.h
@@ -0,0 +1,30 @@
+/*
+ * config.h - This file is part of mxim
+ * Copyright (C) 2024 Matthias Kruk
+ *
+ * Mxim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your
+ * option) any later version.
+ *
+ * Mxim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mxim; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "cmd.h"
+
+#ifndef CONFIG_C
+extern cmd_def_t config_keybindings[255][16];
+#endif /* CONFIG_C */
+
+#endif /* CONFIG_H */
