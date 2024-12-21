448dff75a0311841c874f0b5d3e355f65e4125f9
Matthias Kruk <m@m10k.eu>
2024-12-21 14:11:58 +0900

    segment: Add type to handle parts of the preedit buffer
    CJK input methods typically allow a user to insert characters into
    a preedit buffer, where it is then converted before the IM commits
    the input to the application. However, depending on the amount of
    text in the buffer, it may not be possible to convert the entire
    buffer at once, but it may contain segments (think multiple words)
    that need to be converted individually.
    
    This commit adds the `segment_t` type which represents one unit of
    user input that can be converted as a whole.

---
+++ src/segment.c
@@ -0,0 +1,136 @@
+/*
+ * segment.c - This file is part of mxim
+ * Copyright (C) 2024 Matthias Kruk
+ *
+ * Mxim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your
+ * option) any later version.
+ *
+ * Mxim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mxim; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "char.h"
+#include "segment.h"
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INITIAL_SEGMENT_SIZE 32
+
+int segment_new(segment_t **segment)
+{
+	segment_t *seg;
+
+	if (!(seg = calloc(1, sizeof(*seg)))) {
+		return -ENOMEM;
+	}
+
+	if (!(seg->characters = calloc(INITIAL_SEGMENT_SIZE,
+	                               sizeof(*seg->characters)))) {
+		free(seg);
+		return -ENOMEM;
+	}
+
+	seg->size = INITIAL_SEGMENT_SIZE;
+	*segment = seg;
+	return 0;
+}
+
+int segment_free(segment_t **segment)
+{
+	if (!segment || !*segment) {
+		return -EINVAL;
+	}
+
+	free((*segment)->characters);
+	free(*segment);
+	*segment = 0;
+	return 0;
+}
+
+int segment_erase(segment_t *segment, const short pos)
+{
+	short tail_len;
+
+	if (!segment) {
+		return -EINVAL;
+	}
+
+	if (pos < 0 || pos >= segment->len) {
+		return -EOVERFLOW;
+	}
+
+	tail_len = segment->len - pos - 1;
+	memmove(segment->characters + pos,
+	        segment->characters + pos + 1,
+	        sizeof(*segment->characters) * tail_len);
+
+	segment->len--;
+	segment->characters[segment->len] = CHAR_INVALID;
+
+	return 0;
+}
+
+static int _segment_grow(segment_t *segment)
+{
+	char_t *new_characters;
+	short new_size;
+
+	if (SHRT_MAX - INITIAL_SEGMENT_SIZE < segment->size) {
+		return -EMSGSIZE;
+	}
+
+	new_size = segment->size + INITIAL_SEGMENT_SIZE;
+	if (!(new_characters = realloc(segment->characters,
+	                               new_size * sizeof(*segment->characters)))) {
+		return -ENOMEM;
+	}
+
+	segment->characters = new_characters;
+	segment->size = new_size;
+
+	return 0;
+}
+
+int segment_insert(segment_t *segment, const char_t chr,
+                   const short pos)
+{
+	int err;
+	short tail_len;
+	short insert_pos;
+
+	if (!segment) {
+		return -EINVAL;
+	}
+
+	if (pos < 0) {
+		insert_pos = 0;
+	} else if (pos > segment->len) {
+		insert_pos = segment->len;
+	} else {
+		insert_pos = pos;
+	}
+
+	if (segment->len == (segment->size - 1) &&
+	    ((err = _segment_grow(segment)) < 0)) {
+		return err;
+	}
+
+	tail_len = segment->len - insert_pos;
+	memmove(segment->characters + insert_pos + 1,
+	        segment->characters + insert_pos, tail_len);
+	segment->characters[insert_pos] = chr;
+	segment->len++;
+
+	return 0;
+}
---
+++ src/segment.h
@@ -0,0 +1,43 @@
+/*
+ * segment.h - This file is part of mxim
+ * Copyright (C) 2024 Matthias Kruk
+ *
+ * Mxim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your
+ * option) any later version.
+ *
+ * Mxim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mxim; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef SEGMENT_H
+#define SEGMENT_H
+
+#include "char.h"
+#include <limits.h>
+
+struct segment {
+	char_t *characters;
+	short size;
+	short len;
+};
+
+typedef struct segment segment_t;
+
+#define SEGMENT_START SHRT_MIN
+#define SEGMENT_END   SHRT_MAX
+
+int segment_new(segment_t **segment);
+int segment_free(segment_t **segment);
+int segment_erase(segment_t *segment, const short pos);
+int segment_insert(segment_t *segment, const char_t chr, const short pos);
+
+#endif /* SEGMENT_H */
