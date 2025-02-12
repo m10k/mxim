fd94c90cf249655d4c23e44a174df23510d0afb5
Matthias Kruk <m@m10k.eu>
2025-02-13 08:44:47 +0900

    config: Use CMD_CURSOR_MOVE command to move between segments
    Mxim uses two separate commands to move the cursor inside a segment
    and between segments, even though both can be implemented with the
    same command.
    
    This commit changes the configuration so that the `CMD_CURSOR_MOVE`
    command is used to move the cursor inside a segment as well as
    between segments.

--- src/config.c
+++ src/config.c
@@ -52,13 +52,13 @@ cmd_def_t config_keybindings[255][16] = {
 	},
 	[KEY_LEFT] = {
 		[MOD_NONE]             = { .cmd = CMD_CURSOR_MOVE,    .arg = { .s = { 0, -1 } } },
-		[MOD_CTRL]             = { .cmd = CMD_SEGMENT_MOVE,   .arg = { .i = -1 } },
+		[MOD_CTRL]             = { .cmd = CMD_CURSOR_MOVE,    .arg = { .s = { -1, 0 } } },
 		[MOD_CTRL | MOD_SHIFT] = { .cmd = CMD_SEGMENT_RESIZE, .arg = { .i = -1 } },
 		[MOD_SUPER]            = { .cmd = CMD_CANDIDATE_MOVE, .arg = { .i = -1 } },
 	},
 	[KEY_RIGHT] = {
 		[MOD_NONE]             = { .cmd = CMD_CURSOR_MOVE,    .arg = { .s = { 0, +1 } } },
-		[MOD_CTRL]             = { .cmd = CMD_SEGMENT_MOVE,   .arg = { .i = +1 } },
+		[MOD_CTRL]             = { .cmd = CMD_CURSOR_MOVE,    .arg = { .s = { +1, 0 } } },
 		[MOD_CTRL | MOD_SHIFT] = { .cmd = CMD_SEGMENT_RESIZE, .arg = { .i = +1 } },
 		[MOD_SUPER]            = { .cmd = CMD_CANDIDATE_MOVE, .arg = { .i = +1 } },
 	},
