f4f78b73134fb99bb6b81ab5d91a1790efae6a52
Matthias Kruk <m@m10k.eu>
2025-04-09 23:17:16 +0900

    suggestion: Add license header
    There is no license header in `suggestion.c`, making it unclear
    which license the sources are published under, if the file ever
    gets separated from the rest of the sources.
    
    This commit adds a license header to `suggestion.c`.

--- src/suggestion.c
+++ src/suggestion.c
@@ -1,3 +1,23 @@
+/*
+ * suggestion.c - This file is part of mxim
+ * Copyright (C) 2024-2025 Matthias Kruk
+ *
+ * Mxim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3, or (at your
+ * option) any later version.
+ *
+ * Mxim is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mxim; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #define _POSIX_C_SOURCE 200809L
 
 #include "suggestion.h"
